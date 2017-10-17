21e3b665805ee4bf9df8673005aa46c79c4331c2
Karl <pvial@krypt.local>
2017-10-16 19:05:00 -0700

    pycube v0.5 Major performance improvements included

--- pycube.py
+++ pycube.py
@@ -1,34 +1,34 @@
-import sys
-
-# pycube v0.1.5
+# pycube v0.5
 
 class Cube:
     def __init__(self, key, nonce=""):
-        self.key_list = []
         self.key = ""
         self.master_list = []
         self.alphabet_dict = {}
         self.alphabet_dict_rev = {}
-        for x in range(0,26):
-            self.alphabet_dict[chr(x + 65)] = x
-            self.alphabet_dict_rev[x] = chr(x + 65)
+        self.start_char = 65
+        self.alphabet_size = 26
+        self.size_factor = 3
+        for x in range(0,self.alphabet_size):
+            self.alphabet_dict[chr(x + self.start_char)] = x
+            self.alphabet_dict_rev[x] = chr(x + self.start_char)
 
-        def gen_cube(length, width, depth):
+        def gen_cube(depth, width, length):
             for z in range(0,depth):
                 section_list = []
                 for y in range(0,width):
                     alphabet = []
                     for x in range(0,length):
-                        alphabet.append(chr(x + 65))
+                        alphabet.append(chr(x + self.start_char))
                     for mod in range(0,y):
                         shift = alphabet.pop(0)
                         alphabet.append(shift)
                         shift = alphabet.pop(2)
-                        alphabet.insert(12,shift)
+                        alphabet.insert(44,shift)
                     section_list.append(alphabet)
                 self.master_list.append(section_list)
 
-        gen_cube(26, 26, 26)
+        gen_cube(self.size_factor, self.size_factor, self.alphabet_size)
         self.init(key)
         if nonce != "":
             self.key_cube(nonce)
@@ -37,22 +37,25 @@ class Cube:
         for section in self.master_list:
             for char in key:
                 char_value = self.alphabet_dict[char]
+                sized_pos = char_value % self.size_factor
                 for alphabet in section:
-                    pos = alphabet.index(char)
-                    key_sub = alphabet.pop(pos)
+                    key_sub = alphabet.pop(char_value)
                     alphabet.append(key_sub)
                     for y in range(0,char_value):
                         if y % 2 == 0:
                             shuffle = alphabet.pop(0)
                             alphabet.append(shuffle)
                             shuffle = alphabet.pop(2)
-                            alphabet.insert(12,shuffle)
-                for x in range(char_value):
-                    section = self.master_list.pop(char_value)
-                    newpos = (char_value + (x * 128)) % 26
-                    self.master_list.insert(newpos,section)
+                            alphabet.insert(44,shuffle)
+        for char in key:
+            char_value = self.alphabet_dict[char]
+            sized_pos = char_value % self.size_factor
+            for x in range(char_value):
+                section = self.master_list.pop(sized_pos)
+                self.master_list.append(section)
 
     def load_key(self, skey):
+	self.key_list = []
         self.key = skey
         for element in self.key:
             self.key_list.append(element)
@@ -61,14 +64,11 @@ class Cube:
         sub_key = ""
         for element in key:
             pos = self.alphabet_dict[element]
-            section = self.master_list.pop(pos)
-            sub_alpha = section.pop(pos)
-            shift = sub_alpha.pop(1)
-            sub_alpha.append(shift)
-            section.insert(pos,sub_alpha)
-            self.master_list.insert(pos,section)
+            sized_pos = pos % self.size_factor
+            section = self.master_list[sized_pos]
+            sub_alpha = section[sized_pos]
             sub = sub_alpha.pop(pos)
-            sub_alpha.insert(pos,sub)
+            sub_alpha.append(sub)
             sub_key += sub
         self.load_key(sub_key)
         return sub_key
@@ -78,46 +78,44 @@ class Cube:
         self.key_cube(key)
 
     def morph_cube(self, counter, sub_key):
-        mod_value = counter % 26
-        for key_element in sub_key:
-            key_value = self.alphabet_dict[key_element]
-            shift_value = (mod_value + key_value) % 26
-            for section in self.master_list:
+        mod_value = counter % self.alphabet_size
+        for section in self.master_list:
+            for key_element in sub_key:
+                key_value = self.alphabet_dict[key_element]
+                shift_value = key_value % self.size_factor
                 for alphabet in section:
-                    shift = alphabet.pop(mod_value)
-                    alphabet.insert(shift_value,shift)
-            section_shift = self.master_list.pop(key_value)
+                    alphabet[shift_value], alphabet[mod_value] = alphabet[mod_value], alphabet[shift_value]
+            section_shift = self.master_list.pop(key_value % self.size_factor)
             self.master_list.append(section_shift)
 
     def encrypt(self, words):
         cipher_text = ""
         sub_key = self.key
-        for word in words:
-            for counter, letter in enumerate(word):
-                for section in self.master_list:
-                    for alphabet in section:
-                        sub_pos = self.alphabet_dict[letter]
-                        sub = alphabet.pop(sub_pos)
-                        alphabet.insert(sub_pos,sub)
-                        shift = alphabet.pop(0)
-                        alphabet.append(shift)
-                self.morph_cube(counter, sub_key)
-                sub_key = self.key_scheduler(sub_key)
-                cipher_text += sub
+        for counter, letter in enumerate(words):
+            sub = letter
+            for section in self.master_list:
+                for alphabet in section:
+                    sub_pos = self.alphabet_dict[sub]
+                    sub = alphabet[sub_pos]
+                    shift = alphabet.pop(0)
+                    alphabet.append(shift)
+            sub_key = self.key_scheduler(sub_key)
+            self.morph_cube(counter, sub_key)
+            cipher_text += sub
         return cipher_text
 
     def decrypt(self, words):
         plain_text = ""
         sub_key = self.key
-        for word in words:
-            for counter, letter in enumerate(word):
-                for section in self.master_list:
-                    for alphabet in section:
-                        sub_pos = alphabet.index(letter)
-                        sub = self.alphabet_dict_rev[sub_pos]
-                        shift = alphabet.pop(0)
-                        alphabet.append(shift)
-                self.morph_cube(counter, sub_key)
-                sub_key = self.key_scheduler(sub_key)
-                plain_text += sub
+        for counter, letter in enumerate(words):
+            sub = letter
+            for section in reversed(self.master_list):
+                for alphabet in reversed(section):
+                    sub_pos = alphabet.index(sub)
+                    sub = self.alphabet_dict_rev[sub_pos]
+                    shift = alphabet.pop(0)
+                    alphabet.append(shift)
+            sub_key = self.key_scheduler(sub_key)
+            self.morph_cube(counter, sub_key)
+            plain_text += sub
         return plain_text
